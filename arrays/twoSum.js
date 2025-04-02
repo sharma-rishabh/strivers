07254157ad8385e4ed235cf867239872aa924752
Rishabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2025-04-02 17:59:51 +0530

    two sum

---
+++ arrays/twoSum.js
@@ -0,0 +1,18 @@
+const twoSum = (arr, sum) => {
+  arr.sort((x, y) => x - y);
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+  while (leftIndex < arr.length || rightIndex > 0) {
+    const currSum = arr[leftIndex] + arr[rightIndex];
+    console.log(arr[leftIndex], arr[rightIndex]);
+    if (currSum === sum) return [arr[leftIndex], arr[rightIndex]];
+    if (currSum < sum) {
+      leftIndex++;
+    } else {
+      rightIndex--;
+    }
+  }
+  return [-1, 1];
+};
+
+console.log(twoSum([2, 5, 5, 8, 1], 14));
